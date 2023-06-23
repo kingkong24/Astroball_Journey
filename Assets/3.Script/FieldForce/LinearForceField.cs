307ce84378db18221ba858cbf686143635771959
HoneyBadger34 <129942986+kingkong24@users.noreply.github.com>
2023-06-23 11:20:36 +0900

    ForceField 바람 효과 추가

--- Assets/3.Script/FieldForce/LinearForceField.cs
+++ Assets/3.Script/FieldForce/LinearForceField.cs
@@ -8,21 +8,35 @@ public class LinearForceField : MonoBehaviour
     public float forceAmount = 10f;
     public Vector3 forceDirection = Vector3.forward;
 
+    [Header("효과 설정")]
+    [SerializeField] GameObject[] gameObjects_effect;
+    [SerializeField] float effectCooltime = 0.5f;
+
     [Header("확인용")]
     [SerializeField] FieldForceChecker fieldForceChecker;
     [SerializeField] Rigidbody Rigidbody_player;
+    [SerializeField] Collider collider;
+    [SerializeField] int EffectNum;
+    [SerializeField] int effectCounter;
+    [SerializeField] float activationTimer;
     public bool isPlayerOn;
+
 
     private void Awake()
     {
+        EffectNum = gameObjects_effect.Length;
+        activationTimer = 0;
+        effectCounter = 0;
         isPlayerOn = false;
+        collider = GetComponent<Collider>();
         fieldForceChecker = FindObjectOfType<FieldForceChecker>();
         Rigidbody_player = FindObjectOfType<Rigidbody>();
-
     }
 
     private void Update()
     {
+        EffectActive();
+
         if (isPlayerOn && Rigidbody_player != null)
         {
             Vector3 force = forceDirection.normalized * forceAmount;
@@ -46,5 +60,41 @@ public class LinearForceField : MonoBehaviour
             isPlayerOn = false;
             fieldForceChecker.Recalculate();
         }
+    }
+
+    private void EffectActive()
+    {
+        activationTimer += Time.deltaTime;
+
+        if (activationTimer > effectCooltime)
+        {
+            GameObject effectObject = gameObjects_effect[effectCounter];
+            effectObject.transform.position = GetRandomPosition();
+            transform.rotation = Quaternion.LookRotation(forceDirection);
+
+            effectObject.SetActive(true);
+
+            Rigidbody effectRigidbody = effectObject.GetComponent<Rigidbody>();
+            effectRigidbody.velocity = forceDirection * forceAmount;
+
+            effectCounter++;
+            if(effectCounter >= EffectNum)
+            {
+                effectCounter = 0;
+            }
+            activationTimer = 0;
+        }
+    }
+
+    private Vector3 GetRandomPosition()
+    {
+        Vector3 minPoint = transform.position - collider.bounds.size * 0.5f;
+        Vector3 maxPoint = transform.position + collider.bounds.size * 0.5f;
+
+        float randomX = Random.Range(minPoint.x, maxPoint.x);
+        float randomY = Random.Range(minPoint.y, maxPoint.y);
+        float randomZ = Random.Range(minPoint.z, maxPoint.z);
+
+        return new Vector3(randomX, randomY, randomZ);
     }
 }
