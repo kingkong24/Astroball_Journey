f669def8e01bd7f879eaabc98f02bc21e282e624
HoneyBadger34 <129942986+kingkong24@users.noreply.github.com>
2023-06-27 15:46:17 +0900

    Goal과 player의 거리를 보여주는 UI 추가

--- Assets/3.Script/UI/GoalDistanceUI.cs
+++ Assets/3.Script/UI/GoalDistanceUI.cs
@@ -2,69 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using TMPro;
 
 public class GoalDistanceUI : MonoBehaviour
 {
-    [Header("distanceText")]
-    [SerializeField] TMP_Text distanceText;
+    [SerializeField] Text text_distance;
+    [SerializeField] Transform transform_player;
+    [SerializeField] Transform transform_goal;
 
-    [Header("camera")]
-    [SerializeField] new Camera camera;
-
-    [Header("설정")]
-    [SerializeField] float minSize = 0.0002f;
-    [SerializeField] float maxSize = 0.002f;
-    [SerializeField] float ParentDistance = 100.0f;
-
-    private Transform playerTransform;
-
-    private void OnEnable()
+    private void Awake()
     {
-        Camera cam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
-        if (camera == null)
-        {
-            camera = cam;
-        }
-        else
-        {
-            Debug.LogError("카메라 오브젝트를 찾을 수 없습니다.");
-        }
-
-        GameObject player = GameObject.FindGameObjectWithTag("Player");
-        if (player != null)
-        {
-            playerTransform = player.transform;
-        }
-        else
-        {
-            Debug.LogError("플레이어 오브젝트를 찾을 수 없습니다.");
-        }
+        text_distance = GetComponent<Text>();
     }
 
     private void Update()
     {
-        if (playerTransform == null || camera == null)
+        float distance = Vector3.Distance(transform_player.position, transform_goal.position);
+        if (distance < 100.0f)
         {
-            return;
+            text_distance.text = distance.ToString("00.00") + "m";
         }
-
-        transform.LookAt(transform.position + camera.transform.rotation * Vector3.forward,
-            camera.transform.rotation * Vector3.up);
-
-        float distance = Vector3.Distance(playerTransform.position, transform.position);
-
-        if (distance <100.0f)
+        else if (distance < 1000.0f)
         {
-            distanceText.text = distance.ToString("00.00") + "m";
+            text_distance.text = distance.ToString("000.00") + "m";
         }
         else
         {
-            distanceText.text = distance.ToString("000.00") + "m";
+            text_distance.text = distance.ToString("0000.00") + "m";
         }
-
-        float ratio = Mathf.Clamp01(distance / ParentDistance);
-        float size = Mathf.Lerp(minSize, maxSize, ratio);
-        transform.localScale = new Vector3(20 * size, size, size);
     }
 }
