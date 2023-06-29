50471f784e500d298fc2d49d19b31dcbf1142145
HoneyBadger34 <129942986+kingkong24@users.noreply.github.com>
2023-06-29 09:31:05 +0900

    subGoal에 이벤트 추가

--- Assets/3.Script/InteractiveObjects/SubGoal.cs
+++ Assets/3.Script/InteractiveObjects/SubGoal.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class SubGoal : MonoBehaviour
 {
     [Header("Manager")]
     [SerializeField] StageManager stageManager;
+
+    [Header("이 객체의 콜라이더에 'Player'가 들어왔을 때 발생하는 이벤트")]
+    public UnityEvent Event_ThisSubGoalEnter;
 
     private void Awake()
     {
@@ -21,10 +25,11 @@ public class SubGoal : MonoBehaviour
 
             if (currentTarget == gameObject)
             {
-                
+                Event_ThisSubGoalEnter.Invoke();
                 stageManager.NextTarget();
                 PlayerMovement playerMovement = other.GetComponent<PlayerMovement>();
                 playerMovement.SavePosition = transform.position - new Vector3(0, transform.localPosition.y - 1f, 0);
+
                 gameObject.SetActive(false);
             }
         }
