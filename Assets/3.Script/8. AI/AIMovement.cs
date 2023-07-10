8bdc932493aaa788f960145a915f5654229d5004
HoneyBadger34 <129942986+kingkong24@users.noreply.github.com>
2023-07-10 11:59:35 +0900

    Stage_5 완성?

---
+++ Assets/3.Script/8. AI/AIMovement.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIMovement : MonoBehaviour
+{
+    [Header("타겟")]
+    [SerializeField] Transform transform_target;
+
+    [Header("속도 및 힘")]
+    [SerializeField] float speed = 2.0f;
+    [SerializeField] float angularSpeed = 60.0f;
+    [SerializeField] float force = 500.0f;
+
+    [Header("소리")]
+    [SerializeField] AudioSource audioSource;
+    [SerializeField] AudioClip audioClip;
+
+    [Header("확인용")]
+    [SerializeField] NavMeshAgent navMeshAgent;
+
+    private void Awake()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void Start()
+    {
+        audioSource.volume = GameManager.instance.MasterVolumes * GameManager.instance.SFXVolumes;
+        TrackingStop();
+    }
+
+    private void Update()
+    {
+        navMeshAgent.SetDestination(transform_target.position);
+    }
+
+    /// <summary>
+    /// 타겟을 쫓습니다.
+    /// </summary>
+    public void TrackingStart()
+    {
+        navMeshAgent.speed = speed;
+        navMeshAgent.angularSpeed = angularSpeed;
+    }
+
+    /// <summary>
+    /// 타겟을 쫓기를 멈춥니다.
+    /// </summary>
+    public void TrackingStop()
+    {
+        navMeshAgent.speed = 0;
+        navMeshAgent.angularSpeed = 0;
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.CompareTag("Ball"))
+        {
+            audioSource.PlayOneShot(audioClip);
+            Vector3 forceDirection = new(-col.transform.position.x, 100f, -col.transform.position.z);
+            forceDirection = forceDirection.normalized;
+            col.attachedRigidbody.AddForce(forceDirection * force, ForceMode.Impulse);
+            TrackingStop();
+        }
+    }
+
+}
