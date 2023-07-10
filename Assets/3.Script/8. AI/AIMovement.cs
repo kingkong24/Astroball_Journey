76d49e500fe01de788267ca13e6fa901d83256b4
HoneyBadger34 <129942986+kingkong24@users.noreply.github.com>
2023-07-10 13:25:45 +0900

    Stage_5 완성...

--- Assets/3.Script/8. AI/AIMovement.cs
+++ Assets/3.Script/8. AI/AIMovement.cs
@@ -1,70 +1,94 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.AI;
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using UnityEngine.AI;
 
-public class AIMovement : MonoBehaviour
-{
-    [Header("타겟")]
-    [SerializeField] Transform transform_target;
-
-    [Header("속도 및 힘")]
-    [SerializeField] float speed = 2.0f;
-    [SerializeField] float angularSpeed = 60.0f;
-    [SerializeField] float force = 500.0f;
-
-    [Header("소리")]
-    [SerializeField] AudioSource audioSource;
-    [SerializeField] AudioClip audioClip;
-
-    [Header("확인용")]
-    [SerializeField] NavMeshAgent navMeshAgent;
-
-    private void Awake()
+    public class AIMovement : MonoBehaviour
     {
-        navMeshAgent = GetComponent<NavMeshAgent>();
-        audioSource = GetComponent<AudioSource>();
-    }
+        [Header("타겟")]
+        [SerializeField] Transform transform_target;
 
-    private void Start()
-    {
-        audioSource.volume = GameManager.instance.MasterVolumes * GameManager.instance.SFXVolumes;
-        TrackingStop();
-    }
+        [Header("골프채")]
+        [SerializeField] Transform transform_golfClub;
 
-    private void Update()
-    {
-        navMeshAgent.SetDestination(transform_target.position);
-    }
+        [Header("속도 및 힘")]
+        [SerializeField] float speed = 2.0f;
+        [SerializeField] float angularSpeed = 60.0f;
+        [SerializeField] float force = 500.0f;
+        [SerializeField] float rotateSpeed = 360.0f;
 
-    /// <summary>
-    /// 타겟을 쫓습니다.
-    /// </summary>
-    public void TrackingStart()
-    {
-        navMeshAgent.speed = speed;
-        navMeshAgent.angularSpeed = angularSpeed;
-    }
+        [Header("소리")]
+        [SerializeField] AudioSource audioSource;
+        [SerializeField] AudioClip audioClip;
 
-    /// <summary>
-    /// 타겟을 쫓기를 멈춥니다.
-    /// </summary>
-    public void TrackingStop()
-    {
-        navMeshAgent.speed = 0;
-        navMeshAgent.angularSpeed = 0;
-    }
+        [Header("확인용")]
+        [SerializeField] NavMeshAgent navMeshAgent;
+        [SerializeField] float timer;
+        [SerializeField] Vector3 rotateAxis;
 
-    private void OnTriggerEnter(Collider col)
-    {
-        if (col.CompareTag("Ball"))
+        private void Awake()
         {
-            audioSource.PlayOneShot(audioClip);
-            Vector3 forceDirection = new(-col.transform.position.x, 100f, -col.transform.position.z);
-            forceDirection = forceDirection.normalized;
-            col.attachedRigidbody.AddForce(forceDirection * force, ForceMode.Impulse);
-            TrackingStop();
+            navMeshAgent = GetComponent<NavMeshAgent>();
+            audioSource = GetComponent<AudioSource>();
         }
-    }
 
-}
+        private void Start()
+        {
+            audioSource.volume = GameManager.instance.MasterVolumes * GameManager.instance.SFXVolumes;
+            TrackingStop();
+            StartCoroutine(TurningCrazy());
+        }
+
+        private void Update()
+        {
+            navMeshAgent.SetDestination(transform_target.position);
+        }
+
+        /// <summary>
+        /// 타겟을 쫓습니다.
+        /// </summary>
+        public void TrackingStart()
+        {
+            navMeshAgent.speed = speed;
+            navMeshAgent.angularSpeed = angularSpeed;
+        }
+
+        /// <summary>
+        /// 타겟을 쫓기를 멈춥니다.
+        /// </summary>
+        public void TrackingStop()
+        {
+            navMeshAgent.speed = 0;
+            navMeshAgent.angularSpeed = 0;
+        }
+
+        IEnumerator TurningCrazy()
+        {
+            while(true)
+            {
+                timer += Time.deltaTime;
+                if(timer > 0.5f)
+                {
+                    rotateAxis = Random.insideUnitSphere.normalized;
+                    transform_golfClub.rotation = Quaternion.FromToRotation(Vector3.right, rotateAxis);
+                }
+
+                transform_golfClub.Rotate(Vector3.right, speed * Time.deltaTime);
+
+                yield return null;
+            }
+        }
+
+        private void OnTriggerEnter(Collider col)
+        {
+            if (col.CompareTag("Ball"))
+            {
+                audioSource.PlayOneShot(audioClip);
+                Vector3 forceDirection = new(-col.transform.position.x, 100f, -col.transform.position.z);
+                forceDirection = forceDirection.normalized;
+                col.attachedRigidbody.AddForce(forceDirection * force, ForceMode.Impulse);
+                TrackingStop();
+            }
+        }
+
+    }
